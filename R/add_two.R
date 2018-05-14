a1cb97f63f4d91ad56647c29873a65a1a4c9d251
msf5 <37456720+msf5@users.noreply.github.com>
2018-05-14 14:12:10 +0200

    Add files via upload

---
+++ R/add_two.R
@@ -0,0 +1,15 @@
+#' Add two
+#'
+#' Adds two numbers together
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(123, 456)
+#' add(1.5, 3.14)
+#' @export
+add_two <- function(x, y) {
+  x + y
+}
